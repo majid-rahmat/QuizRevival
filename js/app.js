cfc6d83a0eaaa24f0fffa51d5542a6bf2825c146
naitRAM <rmansieh@gmail.com>
2017-08-27 13:20:26 -0400

    add functionality after submitting question1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 
-
+count=0;
 var zonkImg = "<img src='assets/zonk.png' alt='image of the text zonk' class='zonk'>";
 var cardBtn = '<button class="btn btn-success btn-lg card-btn" type="button"> Take the quiz! </button>';
+
 var shuffleCards = function () {
 	var a = Math.floor(Math.random()*100);
 	console.log(a);
@@ -30,21 +31,33 @@ shuffleCards();
 
 $(".card-btn").click(function (){
 	$(".card-col").hide();
-	$(".question1")
-	var b = document.getElementsByClassName("question1");
+	$(".question")
+	var b = document.getElementsByClassName("question");
     $(b).fadeIn(1000);
 });
 
-$(".question1").on('click', '#correct-answer', function(){
+$(".question").on('click', '#correct-answer', function(){
 	$(this).attr({'class': 'btn btn-lg btn-block btn-success', 'disabled': true, 'type': 'button', 'id': 'correct-answer'});
 	$(this).parent().siblings().find('.wrong').attr({'class': 'btn btn-lg btn-block btn-primary', 'disabled': true});
-	$("#exampleModal").modal('show');
+	$("#modal").modal('show');
+	$(".next-btn").attr("disabled", false);
 });
 
-$(".question1").on('click', '.wrong', function(){
+$(".question").on('click', '.wrong', function(){
 	$(this).attr({'class': 'btn btn-lg btn-block btn-danger', 'disabled': true});
 	$(this).parent().siblings().find('.wrong').attr({'class': 'btn btn-lg btn-block btn-danger', 'disabled': true});
 	$(this).parent().siblings().find('#correct-answer').attr({'class': 'btn btn-lg btn-block btn-success', 'disabled': true, 'type': 'button', 'id': 'correct-answer'});
+	$("#modal").modal('show');
+	$(".next-btn").attr("disabled", false);
+});
+$(".next-btn").click(function() {
+	console.log("fuck");
+	$("#questionPic").attr('src', questions[count+1]["image_q"]);
+	for (var i=0; i<questions[count+1]["choices"].length; i++) {
+	$('#choice'+i).children('button').html(questions[count+1]["choices"][i]);
+}
+	$("#quizQuestion").html(questions[count+1]["question"]);
+	count++;
 });
 
 
