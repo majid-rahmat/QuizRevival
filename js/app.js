fa5c719de495351c3712f482554d9e538a14fa41
naitRAM <rmansieh@gmail.com>
2017-08-28 14:43:11 -0400

    add more code to app.js to improve transition between questionsand display of correct/incorrect answers

--- js/app.js
+++ js/app.js
@@ -36,20 +36,6 @@ $(".card-btn").click(function (){
     $(b).fadeIn(1000);
 });
 
-$(".question").on('click', '#correct-answer', function(){
-	$(this).attr({'class': 'btn btn-lg btn-block btn-success', 'disabled': true, 'type': 'button', 'id': 'correct-answer'});
-	$(this).parent().siblings().find('.wrong').attr({'class': 'btn btn-lg btn-block btn-primary', 'disabled': true});
-	$("#modal").modal('show');
-	$(".next-btn").attr("disabled", false);
-});
-
-$(".question").on('click', '.wrong', function(){
-	$(this).attr({'class': 'btn btn-lg btn-block btn-danger', 'disabled': true});
-	$(this).parent().siblings().find('.wrong').attr({'class': 'btn btn-lg btn-block btn-danger', 'disabled': true});
-	$(this).parent().siblings().find('#correct-answer').attr({'class': 'btn btn-lg btn-block btn-success', 'disabled': true, 'type': 'button', 'id': 'correct-answer'});
-	$("#modal").modal('show');
-	$(".next-btn").attr("disabled", false);
-});
 $(".next-btn").click(function() {
 	console.log("fuck");
 	$("#questionPic").attr('src', questions[count+1]["image_q"]);
@@ -57,10 +43,30 @@ $(".next-btn").click(function() {
 	$('#choice'+i).children('button').html(questions[count+1]["choices"][i]);
 }
 	$("#quizQuestion").html(questions[count+1]["question"]);
+	$(".next-btn").attr("disabled", true);
+	$(".answer-btn").attr({'class': 'btn btn-lg btn-block btn-primary answer-btn', 'disabled': false, 'type': 'button'})
+	$(".modal-body").children("img").attr('src', questions[count+1]["image_a"]);
+	$("#explanation").html(questions[count+1]["explanation"]);
 	count++;
 });
 
-
-
+$(".answer-btn").click(function(){
+	for (var i=0; i<questions[count]["choices"].length; i++) {
+		if ($("#choice"+i).children('button').html() == questions[count]["answer"]) {
+			$("#choice"+i).children('button').attr({'class': 'btn btn-lg btn-block btn-success answer-btn', 'type': 'button'});
+		}
+		else {
+			$("#choice"+i).children('button').attr({'class': 'btn btn-lg btn-block btn-danger answer-btn', 'type': 'button'});
+		}
+	}
+		$("#modal").modal('show');
+		$(".next-btn").attr("disabled", false);
+	if ($(this).html() == questions[count]["answer"]) {
+		$("#ModalLabel").html("Correct!");
+	}
+	else {
+		$("#ModalLabel").html("Incorrect!");
+	}
+});
 
 });
