a66dbb1c72dc39c3a76745074d64c1a4adc3fd24
naitRAM <rmansieh@gmail.com>
2017-08-28 20:07:15 -0400

    complete first draft of quiz app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-count=0;
+var count=0;
+var score = 0;
 var zonkImg = "<img src='assets/zonk.png' alt='image of the text zonk' class='zonk'>";
 var cardBtn = '<button class="btn btn-success btn-lg card-btn" type="button"> Take the quiz! </button>';
 
@@ -23,7 +24,7 @@ var shuffleCards = function () {
 		$('#card2').html(zonkImg);
 		$('#card3').html(cardBtn);
 	}
-}
+};
 
 $(".card").flip();
 
@@ -31,42 +32,57 @@ shuffleCards();
 
 $(".card-btn").click(function (){
 	$(".card-col").hide();
-	$(".question")
 	var b = document.getElementsByClassName("question");
     $(b).fadeIn(1000);
 });
 
 $(".next-btn").click(function() {
-	console.log("fuck");
+	console.log("here kitty kitty");
 	$("#questionPic").attr('src', questions[count+1]["image_q"]);
 	for (var i=0; i<questions[count+1]["choices"].length; i++) {
 	$('#choice'+i).children('button').html(questions[count+1]["choices"][i]);
-}
+	}
 	$("#quizQuestion").html(questions[count+1]["question"]);
 	$(".next-btn").attr("disabled", true);
 	$(".answer-btn").attr({'class': 'btn btn-lg btn-block btn-primary answer-btn', 'disabled': false, 'type': 'button'})
-	$(".modal-body").children("img").attr('src', questions[count+1]["image_a"]);
+	$(".modal-body").find("img").attr('src', questions[count+1]["image_a"]);
 	$("#explanation").html(questions[count+1]["explanation"]);
 	count++;
+	
 });
 
 $(".answer-btn").click(function(){
 	for (var i=0; i<questions[count]["choices"].length; i++) {
 		if ($("#choice"+i).children('button').html() == questions[count]["answer"]) {
-			$("#choice"+i).children('button').attr({'class': 'btn btn-lg btn-block btn-success answer-btn', 'type': 'button'});
+			$("#choice"+i).children('button').attr({'class': 'btn btn-lg btn-block btn-success answer-btn', 'disabled': true, 'type': 'button'});
 		}
 		else {
-			$("#choice"+i).children('button').attr({'class': 'btn btn-lg btn-block btn-danger answer-btn', 'type': 'button'});
+			$("#choice"+i).children('button').attr({'class': 'btn btn-lg btn-block btn-danger answer-btn', 'disabled': true, 'type': 'button'});
 		}
 	}
 		$("#modal").modal('show');
 		$(".next-btn").attr("disabled", false);
 	if ($(this).html() == questions[count]["answer"]) {
-		$("#ModalLabel").html("Correct!");
+		$("#ModalLabel").html("Correct! Your score is " + (score+1) + " out of " + questions.length + "!" );
+		score++;
 	}
 	else {
-		$("#ModalLabel").html("Incorrect!");
+		$("#ModalLabel").html("Incorrect! " + "Your score is " + score + " out of " + questions.length + ".");
 	}
 });
 
+$(".modal").on('click','button', function(){
+	if (count==questions.length-1) {
+		$(".question").hide();
+		$('#score-info').html("Congratulations on completing the quiz! Your score is " + score  + "/" + questions.length + ".");
+		$('.score').show();
+	}
+});
+
+$(".score").on('click','button', function(){
+		$('.score').hide();
+		$(".card").flip(false);
+		$('.card-col').show();
+		shuffleCards();
+});
 });
